4d55cd7418ca8bcfa7cf286b044a846fa9e596d1
andery119 <andery@foxmail.com>
2017-08-23 15:02:15 +0800

    Create demo.php

---
+++ demo.php
@@ -0,0 +1,8 @@
+Iterator extends Traversable {
+    /* 方法 */
+    abstract public mixed current ( void )
+    abstract public scalar key ( void )
+    abstract public void next ( void )
+    abstract public void rewind ( void )
+    abstract public boolean valid ( void )
+}
