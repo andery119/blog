ca2ff1d3597bbe408d453060c1d2792319b1e382
andery119 <andery@foxmail.com>
2017-08-23 15:03:46 +0800

    Update demo.php

--- demo.php
+++ demo.php
@@ -1,8 +1,2 @@
-Iterator extends Traversable {
-    /* 方法 */
-    abstract public mixed current ( void )
-    abstract public scalar key ( void )
-    abstract public void next ( void )
-    abstract public void rewind ( void )
-    abstract public boolean valid ( void )
-}
+$number = range(0,5);
+print_r($number);
